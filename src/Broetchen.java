5867f3e8103b7e99d1edcce816a53eef6df139db
nextfailed <lars.huebner@proton.me>
2026-05-17 18:49:25 +0200

    Add Broetchen und Zutaten

---
+++ src/Broetchen.java
@@ -0,0 +1,30 @@
+public class Broetchen extends Zutat{
+    int backzeit;
+    int hoehe;
+
+    public Broetchen(int nummer, String name, float preis, boolean klassisch, boolean vegan, boolean vegetarisch, int backzeit, int hoehe) {
+        super(nummer, name, preis, klassisch, vegan, vegetarisch);
+        this.backzeit = backzeit;
+        this.hoehe = hoehe;
+    }
+
+    public Broetchen(int nummer, String name, float preis, int backzeit, int hoehe){
+        this(nummer,name,preis,false,false,false,backzeit,hoehe);
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + " | Backzeit" + this.zubereiten();
+    }
+
+    @Override
+    public int zubereiten() {
+        
+        return hoehe * backzeit;
+    }    
+
+    @Override
+    public float berechneHoehe(){
+        return (float)((this.hoehe * 0.25)*backzeit);
+    }
+}
---
+++ src/Zutat.java
@@ -0,0 +1,63 @@
+public abstract class Zutat{
+    protected int nummer;
+    protected String name;
+    protected float preis;
+
+    protected boolean klassisch; 
+    protected boolean vegan;
+    protected boolean vegetarisch;
+
+    public Zutat(int nummer, String name, float preis, boolean klassisch, boolean vegan, boolean vegetarisch){
+        this.nummer = nummer;
+        this.name = name;
+
+        this.preis = preis;
+        this.klassisch = klassisch;
+
+        this.vegan = vegan; 
+        this.vegetarisch = vegetarisch;
+    }
+
+    public Zutat(int nummer, String name, float preis){
+        this(nummer,name, preis, false, false, false);
+    }
+
+    @Override
+    public String toString(){
+        String isVegan = vegan?"(vegan) ":"";
+        String isVegetarisch = vegetarisch?"(vegetarisch) ":"";
+        String isKlassisch = klassisch?"(klassisch) ":"";
+
+        return name + " : " + nummer + ")" + isVegan + isVegetarisch + isKlassisch;
+    }
+
+    public abstract int zubereiten();
+
+    public abstract float berechneHoehe();
+
+    // Setter
+    public void setVegan(boolean newValue){
+        this.vegan = newValue;
+    }
+
+    public void setVegetarisch(boolean newValue){
+        this.vegetarisch = newValue;
+    }
+
+    public void setClassic(boolean newValue){
+        this.klassisch = newValue;
+    }
+
+    // Getter
+    public boolean getVegan(boolean newValue){
+        return this.vegan;
+    }
+
+    public boolean getVegetarisch(boolean newValue){
+        return this.vegetarisch;
+    }
+
+    public boolean getKlassisch(boolean newValue){
+        return this.klassisch;
+    }
+}
