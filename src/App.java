14dab6c753fabd1d0ced18600efbc2d6a6a954f3
anrittel <andre.rittelmeyer@student.hs-rm.de>
2026-05-25 23:30:42 +0200

    Katalog und Zutatenueberarbeitung

--- src/App.java
+++ src/App.java
@@ -121,21 +121,37 @@ public class App {
 
     /**
      * Erzeugt den Standartkatalog, der von Anfang an existiert
-     * Zutaten werden autoamtisch in der ArrayListe innerhalb der Zutatenklasse deklariert und muessen daher nur instanziiert werden
+     * Zutaten werden autoamtisch in der ArrayListe<Zutat> innerhalb der Zutatenklasse deklariert und muessen daher nur instanziiert werden
      */
     public static void generiereKatalog() {
 
         // Broetchen
-        new Broetchen(10, "Hamburger (Standard)", 0.85f, true, Zutat.INDIKATOR_VEGETARISCH, 27, 90);
+        new Broetchen(10, "Hamburger (Standard)", 0.85f, 27, 90).setVegetarisch().setClassic(true);
+        new Broetchen(11, "Hamburger Sesam", 0.95f, 90, 27).setVegetarisch().setClassic(true);
+        new Broetchen(12, "Vegan-Broetchen", 0.55f, 240, 34).setVegan();
+        new Broetchen(12, "Ciabatta", 0.45f, 330, 41).setVegetarisch();
 
         // Bratlinge
-        new Bratling(20, "Rindfleisch (Original)", 1.85f, true, Zutat.INDIKATOR_NON_VEGAN, 270, 25);
+        new Bratling(20, "Rindfleisch (Original)", 1.85f, 270, 25).setNonVegan().setClassic(true);
+        new Bratling(21, "Haenchenfleisch gegrillt", 1.15f,  180, 11).setNonVegan().setClassic(true);
+        new Bratling(22, "Falafel-Bratling", 1.45f, 210, 21).setVegan().setClassic(true);
+        new Bratling(23, "Gemuese Bratling", 1.75f, 240, 25).setVegetarisch().setClassic(true);
 
         // Gemuese
+        new Gemuese(40, "Tomate", 0.25f, 3, 3).setClassic(true);;
+        new Gemuese(41, "Salzgurke", 0.15f, 4, 2).setClassic(true);
+        new Gemuese(42, "Rote Zwiebelringe", 0.08f, 5, 2);
+        new Gemuese(43, "Jalapeno-Ringe", 0.08f, 5, 2);
 
         // Salat
+        new Salat(30, "Eisbergsalat", 0.18f);
+        new Salat(31, "Roculasalat", 0.25f);
 
         // Sauce
+        new Sauce(50, "Ketchup", 0.10f, 10, Geschmack.NORMAL).setVegan().setClassic(true);
+        new Sauce(51, "Sandwich-Sauce", 0.15f, 10, Geschmack.NORMAL).setVegetarisch().setClassic(true);
+        new Sauce(52, "Chili-Sauce", 0.25f, 8, Geschmack.SCHARF).setVegan();
+        new Sauce(53, "Honig-Senf-Sauce", 0.18f, 8, Geschmack.SUESS).setVegetarisch();
 
         // 
     }
