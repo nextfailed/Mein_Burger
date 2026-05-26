6c189748c4e92df442234763122cc3b9c760ddf9
anrittel <andre.rittelmeyer@student.hs-rm.de>
2026-05-26 13:05:49 +0200

    Kommentare fuer Geschmack und Geschmackupgrade

--- src/Geschmack.java
+++ src/Geschmack.java
@@ -1,17 +1,30 @@
+import java.awt.Color;
+
+/**
+ * Geschmack fuer Saucen aber auch andere Klassen falls neue ergaenzt werden besitzen einen Geschmack als Enum.
+ * Geschmaecker besitzen ihren eigenen Geschmack als String-Ausgabe und wird direkt als solches durch die toString-Methode mit ihrer jeweiligen Farbe direkt ausgegeben
+ */
 public enum Geschmack {
         NORMAL("-"),
-        SCHARF("Scharf"),
-        SUESS("Suess"),
-        SALZIG("Salzig");
+        SCHARF("Scharf", Color.RED),
+        SUESS("Suess", Color.MAGENTA),
+        SALZIG("Salzig", Color.BLUE);
 
         String beschreibung;
+        Color farbrepraesentierung;
+        static final DyeBucket bucket = new DyeBucket();
+
+        private Geschmack(String name, Color color){
+                this.beschreibung = name;
+                this.farbrepraesentierung = color;
+        }
 
         private Geschmack(String name){
-                this.beschreibung = name;
+                this(name, Color.WHITE);
         }
 
         @Override
         public String toString(){
-                return this.beschreibung;
+                return bucket.dyeText(this.beschreibung, farbrepraesentierung);
         }
 }
--- src/hatGeschmack.java
+++ src/hatGeschmack.java
@@ -1,3 +1,7 @@
+/**
+ * Das hatGeschmack-Interface ermoeglicht, alle Objekte zu sammeln, die einen Geschmack besitzen.
+ * Dies ist hauptsaechlich wichtig fuer die Ausgabe von Geschmack.
+ */
 public interface hatGeschmack {
     
     public abstract Geschmack getGeschmack();
